ca058a01213fbe145ec92ce894317f5170bbdc98
Tom Glover <mrethol@gmail.com>
2026-04-20 16:13:52 +0200

    React hello world example

---
+++ Lecture14/reactHelloWorld/App.jsx
@@ -0,0 +1,10 @@
+import Hello from './components/Hello/Hello'
+
+export default function App() {
+    return (
+        <div>
+            <h1>My App</h1>
+            <Hello name="World" />
+        </div>
+    )
+}
---
+++ Lecture14/reactHelloWorld/components/hello/Hello.jsx
@@ -0,0 +1,5 @@
+import './Hello.css'
+
+export default function Hello({ name }) {
+    return <div className="hello">Hello, {name}</div>
+}
